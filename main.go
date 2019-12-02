7d2c365640d2c537867ad8a62de8d9cc80516a2a
fabiochr <fnatec@gmail.com>
2019-12-01 19:18:32 -0500

    changes made

--- main.go
+++ main.go
@@ -9,13 +9,13 @@ import (
 func main(){
 	http.HandleFunc("/", Index)
 	http.ListenAndServe(":5000", nil)
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatalf("Error opening database: %q", err)
+	}	
 }
 
 func Index(w http.ResponseWriter, r *http.Request){
 	log.Println("Sucesso")
 }
 
-db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
-if err != nil {
-	log.Fatalf("Error opening database: %q", err)
-}
